8bfcf825d56e53456ec1ffb20d6b8d9dba884c2c
iammarkps <markkung08@gmail.com>
2020-05-20 20:02:15 +0700

    Add gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import Router from 'next/router'
+import * as gtag from '../lib/gtag'
 
 import { CSSReset, ThemeProvider } from '@chakra-ui/core'
 import { GlobalStyle } from '../design'
 import { customTheme } from '../design/theme'
 
-const App: NextPage<AppProps> = ({ Component, pageProps }) => (
-  <React.StrictMode>
-    <Head>
-      <title>ประกาศห้องเรียน | โรงเรียนเตรียมอุดมศึกษา</title>
-    </Head>
-    <ThemeProvider theme={customTheme}>
-      <CSSReset />
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  </React.StrictMode>
-)
+const App: NextPage<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    Router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
+  return (
+    <React.StrictMode>
+      <Head>
+        <title>ประกาศห้องเรียน | โรงเรียนเตรียมอุดมศึกษา</title>
+      </Head>
+      <ThemeProvider theme={customTheme}>
+        <CSSReset />
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.StrictMode>
+  )
+}
 
 export default App
